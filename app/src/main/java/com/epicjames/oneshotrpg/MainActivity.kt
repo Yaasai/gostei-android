1b52a02dbd10a232face37b1b444fb7827e244ec
Juan Suman <victor.suman23@gmail.com>
2020-12-03 19:03:24 -0300

    tela inicial feita

--- app/src/main/java/com/epicjames/oneshotrpg/MainActivity.kt
+++ app/src/main/java/com/epicjames/oneshotrpg/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.epicjames.oneshotrpg
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSignUp.setBackgroundColor(Color.parseColor("#4C3377"))
+        buttonLogin.setBackgroundColor(Color.parseColor("#FFFFFF"))
     }
 }
