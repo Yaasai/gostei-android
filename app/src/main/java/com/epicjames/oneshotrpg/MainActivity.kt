8049e62fb484b2b7f09158306cdc75c0f83a4c61
Juan Suman <victor.suman23@gmail.com>
2020-12-04 14:36:55 -0300

    autenticação pronta

--- app/src/main/java/com/epicjames/oneshotrpg/MainActivity.kt
+++ app/src/main/java/com/epicjames/oneshotrpg/MainActivity.kt
@@ -3,30 +3,57 @@ package com.epicjames.oneshotrpg
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonEntrar.setBackgroundColor(Color.parseColor("#4C3377"))
 
         try {
             this.supportActionBar!!.hide()
-        } catch (e: NullPointerException) {}
-
-        setContentView(R.layout.activity_main)
-
-        buttonRegistrar.setBackgroundColor(Color.parseColor("#4C3377"))
-        buttonEntrar.setBackgroundColor(Color.parseColor("#FFFFFF"))
-
-        buttonRegistrar.setOnClickListener {
-            val i = Intent(this, RegistrarActivity::class.java)
-            startActivity(i)
+        } catch (e: NullPointerException) {
         }
 
         buttonEntrar.setOnClickListener {
-            val i = Intent(this, EntrarActivity::class.java)
-            startActivity(i)
+            if (getCurrentUser() == null) {
+                val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(), 0
+                )
+            }
+            else {
+                Toast.makeText(this, "Já autenticado!", Toast.LENGTH_LONG).show()
+            }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0) {
+            if (resultCode == AppCompatActivity.RESULT_OK) {
+                Toast.makeText(this, "Já autenticado!", Toast.LENGTH_LONG).show()
+            }
+            else {
+                finishAffinity()
+            }
+        }
+    }
+
+    private fun getCurrentUser(): FirebaseUser? {
+        val auth = FirebaseAuth.getInstance()
+        return auth.currentUser
     }
 }
