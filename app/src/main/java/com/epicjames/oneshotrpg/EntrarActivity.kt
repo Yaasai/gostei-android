0437f4c470c31986ed46886965b0db5d92d71560
Juan Suman <victor.suman23@gmail.com>
2020-12-04 12:15:55 -0300

    adicionando firebase auth

--- app/src/main/java/com/epicjames/oneshotrpg/EntrarActivity.kt
+++ app/src/main/java/com/epicjames/oneshotrpg/EntrarActivity.kt
@@ -7,7 +7,7 @@ class EntrarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entrar)
-        
+
         val actionbar = supportActionBar
         if (actionbar != null) {
             actionbar.setDisplayHomeAsUpEnabled(true)
